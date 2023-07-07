fa3239512b9beaf1e94bf39e6f2dd7e57b18c53d
Louis Pahlavi <louis.pahlavi@mail.utoronto.ca>
2023-07-07 20:05:11 +0200

    Add message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero";
 import { HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,20 +9,21 @@ import { HeroService} from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes: Hero[] = [];
   selectedHero? : Hero;
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
   }
 }
