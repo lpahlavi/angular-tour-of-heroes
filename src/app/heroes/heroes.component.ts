e08b1346b1b05c37a05409e3a3bb93944692a4b2
Louis Pahlavi <louis.pahlavi@ergon.ch>
2023-08-07 14:28:10 -0400

    Routing to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,29 +1,21 @@
-import { Component } from '@angular/core';
-import { Hero } from "../hero";
-import { HeroService} from "../hero.service";
-import {MessageService} from "../message.service";
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  selectedHero? : Hero;
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
-
+  constructor(private heroService: HeroService) {}
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit () {
+    this.getHeroes();
   }
 }
